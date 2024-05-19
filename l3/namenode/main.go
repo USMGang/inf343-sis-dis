09b2780c7c9bb8e1ba305be0ec468c87f319a166
juan.arias <juan.ariasr@sansano.usm.cl>
2024-05-19 13:40:44 -0400

    asd

--- l3/namenode/main.go
+++ l3/namenode/main.go
@@ -8,35 +8,33 @@ import (
 	"os"
 	"strconv"
 
-	"google.golang.org/grpc"
 	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
 )
 
-func main(){
-	n_mercenaries, _:= strconv.Atoi(os.Args[1])
+func main() {
+	n_mercenaries, _ := strconv.Atoi(os.Args[1])
 
-    // ================== Inicializar el servidor ==================
-    err := godotenv.Load()
-    g.FailOnError(err, "Error al cargar el archivo .env")
+	// ================== Inicializar el servidor ==================
+	err := godotenv.Load()
+	g.FailOnError(err, "Error al cargar el archivo .env")
 
-    namenodeHost := os.Getenv("NAMENODE_HOST")
-    namenodePort := os.Getenv("NAMENODE_PORT")
+	namenodeHost := os.Getenv("NAMENODE_HOST")
+	namenodePort := os.Getenv("NAMENODE_PORT")
 
-    ip_conn := fmt.Sprintf("%s:%s", namenodeHost, namenodePort)
-    lis, err := net.Listen("tcp", ip_conn)
-    g.FailOnError(err, fmt.Sprintf("Error, no se pudo establece el listener en: %s", ip_conn))
+	ip_conn := fmt.Sprintf("%s:%s", namenodeHost, namenodePort)
+	lis, err := net.Listen("tcp", ip_conn)
+	g.FailOnError(err, fmt.Sprintf("Error, no se pudo establece el listener en: %s", ip_conn))
 
-    fmt.Println("Servidor gRPC iniciado en: ", ip_conn)
+	fmt.Println("Servidor gRPC iniciado en: ", ip_conn)
 
-    s := namenode.Server{}
-    s.InitServer(n_mercenaries)
-    grpcServer := grpc.NewServer()
+	s := namenode.Server{}
+	s.InitServer(n_mercenaries)
+	grpcServer := grpc.NewServer()
 
+	namenode.RegisterNamenodeServiceServer(grpcServer, &s)
 
-    namenode.RegisterNamenodeServiceServer(grpcServer, &s)
-
-    err = grpcServer.Serve(lis)
-    g.FailOnError(err, fmt.Sprintf("Error, no se pudo establecer el servidor gRPC en: %s", ip_conn))
-
+	err = grpcServer.Serve(lis)
+	g.FailOnError(err, fmt.Sprintf("Error, no se pudo establecer el servidor gRPC en: %s", ip_conn))
 
 }
