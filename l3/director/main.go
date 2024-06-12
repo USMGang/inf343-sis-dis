31ca2fb72c17aa809de3d479e104c3d9eafa3121
juan.arias <juan.ariasr@sansano.usm.cl>
2024-06-11 21:30:44 -0400

    sos

--- l3/director/main.go
+++ l3/director/main.go
@@ -10,40 +10,41 @@ import (
 )
 
 var (
-    choice = 0
-    s = f.Server{}
-    fs = FloorsServers{}
-    quit = make(chan bool)
+	choice = 0
+	s      = f.Server{}
+	fs     = FloorsServers{}
+	quit   = make(chan bool)
 )
 
-func main(){
-    N_MERCENARIES, _ := strconv.Atoi(os.Args[1])
+// comment
+func main() {
+	N_MERCENARIES, _ := strconv.Atoi(os.Args[1])
 
-    // ================== Inicializar el servidor ==================
-    doshHost := "10.35.169.80"
-    doshPort := "8081"
+	// ================== Inicializar el servidor ==================
+	doshHost := "10.35.169.80"
+	doshPort := "8081"
 
-    directorHost := "10.35.169.79"
-    directorPort := "8080"
+	directorHost := "10.35.169.79"
+	directorPort := "8080"
 
-    s.InitServer(N_MERCENARIES)
-    defer s.Dosh.Conn.Close()
-    defer s.Dosh.Ch.Close()
+	s.InitServer(N_MERCENARIES)
+	defer s.Dosh.Conn.Close()
+	defer s.Dosh.Ch.Close()
 
-    fs.initDoshbank(doshHost, doshPort)
+	fs.initDoshbank(doshHost, doshPort)
 
-    s.Ui = u.NewUI(g.N_NOTIFICATIONS)
-    s.Ui.ChangeOptions(g.DIRECTOR_PROMPT, g.DIRECTOR_OPTIONS)
-    s.Ui.InitInterfaceChoice()
-    showInterface()
+	s.Ui = u.NewUI(g.N_NOTIFICATIONS)
+	s.Ui.ChangeOptions(g.DIRECTOR_PROMPT, g.DIRECTOR_OPTIONS)
+	s.Ui.InitInterfaceChoice()
+	showInterface()
 
-    fs.setListener(directorHost, directorPort, &s)
+	fs.setListener(directorHost, directorPort, &s)
 
-    // Interfaz del director
-    go func(){
-        <-quit
-        fs.lis.Close()
-        fs.grpcServer.GracefulStop()
-        os.Exit(0)
-    }()
+	// Interfaz del director
+	go func() {
+		<-quit
+		fs.lis.Close()
+		fs.grpcServer.GracefulStop()
+		os.Exit(0)
+	}()
 }
